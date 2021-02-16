cf157a1e7b40c8bcb3e7bbcfa713785e8756424c
Chaojun Tan <495825947@qq.com>
2021-02-16 20:03:52 +0800

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,8 +13,12 @@ EOF
 
 /usr/local/bin/caddy --conf=/etc/caddy/Caddyfile &
 /usr/local/bin/filebrowser -r /usr/local/bin/himawari-rx/src/received -p 9999 -a 0.0.0.0 &
+
 cd /usr/local/bin/himawari-rx/src
-./start.sh
+#./start.sh
+./himawari-rx__auto.sh &
+./time_monitor_to_terminate_TSDuck.sh
+
 
 
 
