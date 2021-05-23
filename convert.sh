44a4f9df2e13e26d922ae848084d4f78170306b5
Chaojun Tan <495825947@qq.com>
2021-05-23 16:35:12 +0800

    Add files via upload

--- convert.sh
+++ convert.sh
@@ -5,12 +5,55 @@
 
 
 
+
+
+stringLength() {
+len=`echo "$1" | awk '{printf("%d", length($0))}'`
+echo "$len"
+}
+
+getFileTime(){
+Time="$1"
+
+Hours="$(expr $Time / 100)"
+Minutes="$(expr $Time % 100)"
+
+Minutes_5="$(expr $Minutes / 10 % 10)"
+
+if [ "$Minutes_5" == "5" ]; then
+result="$(expr $(expr $Hours + 1) \* 100)"
+else
+result="$(expr $Time / 10 \* 10 + 10)"
+fi
+
+len=`stringLength "$result"`
+[ "$len" == "3" ] && result="0$result"
+[ "$len" == "2" ] && result="00$result"
+[ "$len" == "1" ] && result="000$result"
+[ "$len" == "0" ] && result="0000$result"
+
+echo "$result"
+}
+
+
+
+
+
+
+
+
+
+
 # Vars
 DATE="$1"
 [ -z "$DATE" ] && DATE="$(date +%Y%m%d)"
 
-Pic_Dir="/usr/local/bin/himawari-rx/src/received/$DATE"
-Gif_Dir="$Pic_Dir/IMG_Merged"
+Time="$2"
+[ -z "$Time" ] && Time="$(date +%H%M)"
+FileTime=`getFileTime "$Time"`
+
+Pic_Dir="/usr/local/bin/himawari-rx/src/received/${DATE}"
+Gif_Dir="${Pic_Dir}/IMG_Merged"
 
 
 
@@ -20,11 +63,13 @@ Gif_Dir="$Pic_Dir/IMG_Merged"
 
 
 # Resize
-mkdir -p /tmp/resize_$DATE
-cd $Pic_Dir
-for IMG in $(ls *.png); do
-  convert -resize "1080x1080" -strip -quality 100% $IMG /tmp/resize_$DATE/$IMG;
-done
+mkdir -p /tmp/resize_${DATE}_${FileTime}
+cd ${Pic_Dir}
+for IMG in $(ls *.png);
+ do
+  #convert -resize "800x800" -strip -quality 75% ${IMG} /tmp/resize_${DATE}_${FileTime}/${IMG};
+  convert -resize "1080x1080" -strip -quality 100% ${IMG} /tmp/resize_${DATE}_${FileTime}/${IMG};
+ done
 
 
 
@@ -34,15 +79,15 @@ done
 
 
 # Merged
-mkdir -p $Gif_Dir
+mkdir -p ${Gif_Dir}
 
-#convert -delay 24 -loop 0 /tmp/resize_$DATE/*.png $Gif_Dir/0000-2400_$DATE.gif
-cd /tmp/resize_$DATE
+#convert -delay 24 -loop 0 /tmp/resize_${DATE}_${FileTime}/*.png ${Gif_Dir}/0000-${FileTime}_${DATE}.gif
+cd /tmp/resize_${DATE}_${FileTime}
 python3 /opt/gif.py
-mv created_gif.gif $Gif_Dir/0000-2400_$DATE.gif
-cd $Pic_Dir
+mv created_gif.gif $Gif_Dir/0000-${FileTime}_${DATE}.gif
+cd ${Pic_Dir}
 
-rm -rf /tmp/resize_$DATE
+rm -rf /tmp/resize_${DATE}_${FileTime}
 
 
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,45 @@
 
 
 
+if [ ! -z "$CONVERT_TIMES" ]; then
+rm -rf /tmp/crontab
+touch /tmp/crontab
+rm -rf /tmp/crontab_
+touch /tmp/crontab_
+echo "$CONVERT_TIMES" | sed "s/,/\n/gi" | while read LINE; do
+if [ ! -z "$LINE" ]; then
+len=`stringLength "$LINE"`
+index=`expr $len - 2`
+Hours="${LINE:0:$index}"
+Minutes="${LINE:$index:2}"
+
+[ -z "$Hours" ] && Hours="23"
+[ -z "$Minutes" ] && Minutes="59"
+
+if [ "$Minutes" -eq "00" ] || [ "$Minutes" -eq "0" ]; then
+Hours=`expr $Hours - 1`
+len_Hours=`stringLength "$Hours"`
+[ "$len_Hours" -lt 2 ] && Hours="0$Hours"
+Minutes="59"
+else
+Minutes="${Minutes:0:1}9"
+fi
+len_Minutes=`stringLength "$Minutes"`
+[ "$len_Minutes" -lt 2 ] && Minutes="0$Minutes"
+
+echo "$Minutes $Hours * * * /opt/convert.sh &" >> /tmp/crontab_
+fi
+done
+cat /tmp/crontab_ | sort -u >> /tmp/crontab
+crontab /tmp/crontab
+rm -rf /tmp/crontab
+rm -rf /tmp/crontab_
+fi
+
+
 /etc/init.d/cron restart
+
+
 
 
 
@@ -65,10 +103,13 @@ rm -r /tmp/resize_*
 
 
 
+#[ -z "$DEVICE" ] && DEVICE="/dev/dvb/adapter0"
+
+
 cd /usr/local/bin/himawari-rx/src
 
-#./start.sh
-./himawari-rx__auto.sh &
+#./start.sh "$DEVICE"
+./himawari-rx__auto.sh "$DEVICE" &
 ./time_monitor_to_terminate_TSDuck.sh
 
 
